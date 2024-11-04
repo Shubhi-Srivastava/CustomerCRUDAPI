4cad5d47fd8d363a37db5e04b2dd80c14a7ca321
Shubhi-Srivastava <shubhi2898srivastava@gmail.com>
2024-11-03 21:47:20 -0800

    Added GlobalExceptioHandler to handle exceptions and map status code to exceptions

---
+++ CustomerAPI/src/main/java/com/test/CustomerAPI/controller/GlobalExceptionHandler.java
@@ -0,0 +1,35 @@
+package com.test.CustomerAPI.controller;
+
+import com.test.CustomerAPI.exception.CustomerNotFoundException;
+import com.test.CustomerAPI.exception.DuplicateEmailException;
+import com.test.CustomerAPI.exception.InvalidCustomerDataException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+
+
+    @ExceptionHandler(CustomerNotFoundException.class)
+    public ResponseEntity<String> handleCustomerNotFoundException(CustomerNotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(InvalidCustomerDataException.class)
+    public ResponseEntity<String> handleInvalidCustomerDataException(InvalidCustomerDataException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleGenericException(Exception ex) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("An unexpected error occurred.");
+    }
+    @ExceptionHandler(DuplicateEmailException.class)
+    public ResponseEntity<String> handleDuplicateEmailException(DuplicateEmailException ex) {
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(ex.getMessage());
+    }
+
+}
